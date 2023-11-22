659c381dea0a280b3d034b0a8733d7f2de134afd
Aan Maryanto <maryanto.aan@gmail.com>
2023-11-22 20:12:19 +0700

    initial commit

---
+++ src/main/java/com/example/xsisminiproject/XsisMiniprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.xsisminiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XsisMiniprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XsisMiniprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/xsisminiproject/XsisMiniprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xsisminiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XsisMiniprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
